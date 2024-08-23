542793e6970787f6cf160ceafee70c033ed5eded
sorakazi <145249696+sorakazi@users.noreply.github.com>
2024-08-24 01:42:44 +0800

    update

--- src/components/LoaderSpinner/LoaderSpinner.jsx
+++ src/components/LoaderSpinner/LoaderSpinner.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { StyledLoader } from './StyledLoaderSpinner';
+import React from "react";
+import { Triangle } from "react-loader-spinner"; // Correct import from react-loader-spinner
 
 const LoaderSpinner = () => {
   return (
-    <StyledLoader>
-      <div className={'lds-default'}>
-        {[...Array(12)].map((_, index) => (
-          <div key={index}></div>
-        ))}
-      </div>
-    </StyledLoader>
+    <Triangle
+      height="80"
+      width="80"
+      color="#4fa94d"
+      ariaLabel="triangle-loading"
+      wrapperStyle={{ margin: "auto", display: "block" }}
+      visible={true} // Make sure the spinner is visible
+    />
   );
 };
 
